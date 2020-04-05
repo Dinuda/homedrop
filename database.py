ea94e8bd155337c3e952f21b764f40ef97955235
Hasith Yaggahavita <hasith@gmail.com>
2020-04-05 21:25:30 +0530

    layout javascripts fixed

--- database.py
+++ database.py
@@ -4,8 +4,8 @@ from flask_mysqldb import MySQL
 def getDatabase(app):
     app.config['MYSQL_HOST'] = 'localhost'
     app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'M@hpshinyblack'
-    #app.config['MYSQL_PASSWORD'] = 'mysqlroot'
+    #app.config['MYSQL_PASSWORD'] = 'M@hpshinyblack'
+    app.config['MYSQL_PASSWORD'] = 'mysqlroot'
     app.config['MYSQL_DB'] = 'home_delivery'
 
     return MySQL(app)  
