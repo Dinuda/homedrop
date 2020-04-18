28c4bd34f021bf50ba66996b1b203a3b92050441
unknown <ydinuda@gmail.com>
2020-04-18 11:47:06 +0530

    Website fully completed

--- application.py
+++ application.py
@@ -11,12 +11,12 @@ import index
 import vendor
 import company
 
-app = Flask(__name__,  static_url_path='',
+application = Flask(__name__,  static_url_path='',
             static_folder='static', template_folder='templates')
 
-mysql = database.getDatabase(app)
+mysql = database.getDatabase(application)
 
-@app.context_processor
+@application.context_processor
 def inject_filter_menu():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM home_delivery.locations ORDER BY location ;")
@@ -30,23 +30,23 @@ def inject_filter_menu():
 
 
 # ?category=1&location=2
-@app.route('/')
+@application.route('/')
 def homePage():
     locationId = request.args.get("location")
     categoryId = request.args.get("category")
     return index.homepage(mysql, locationId, categoryId)
 
 
-@app.route('/vendor')
+@application.route('/vendor')
 def vendorDetail():
     vendorid = request.args.get("id")
     return vendor.vendorDetail(mysql ,vendorid)
 
-@app.route('/newuser', methods=['GET', 'POST'])
+@application.route('/newuser', methods=['GET', 'POST'])
 def newuser():
     return render_template('user_signup.html')
 
-@app.route('/user_reg', methods=['POST'])
+@application.route('/user_reg', methods=['POST'])
 def user_reg():
     name = request.form['name']
     email = request.form['email']
@@ -58,7 +58,7 @@ def user_reg():
     mysql.connection.commit()   
     cursor.close() 
 
-@app.route('/submit_insert_location',methods=['POST'])
+@application.route('/submit_insert_location',methods=['POST'])
 def company_location():
     location = request.form['location']
     vendor = request.form['vendor']
@@ -68,18 +68,18 @@ def company_location():
     cur.close()
     return redirect('/edit_company?id=' + str(vendor)) 
 
-@app.route('/delete_location' , methods=['GET'])
+@application.route('/delete_location' , methods=['GET'])
 def delete_location():
     id = request.args.get("id")
     vendor_id = request.args.get("vendorid")
     return company.editDeleteLocation(mysql, id, vendor_id)
 
-@app.route('/edit_company')
+@application.route('/edit_company')
 def create_company():
     id = request.args.get("id")
     return company.editCompany(mysql, id)
 
-@app.route('/submit_edit_company' , methods=['POST'])
+@application.route('/submit_edit_company' , methods=['POST'])
 def new_comp():
     name = request.form['name']
     site = request.form['site']
@@ -87,14 +87,14 @@ def new_comp():
     id = request.form.get('id')                 
     return company.editSubmit(mysql, name, site, category, id)
 
-@app.route('/submit_flyer_edit_company', methods= ['POST'])
+@application.route('/submit_flyer_edit_company', methods= ['POST'])
 def submit_flyer_edit_company():
     id = request.form['id']
     upload = request.files['upload']
     return company.editFlyer(mysql,id, upload)
 
 
-@app.route('/submit_contacts_edit_company' , methods= ['POST'])
+@application.route('/submit_contacts_edit_company' , methods= ['POST'])
 def submit_contacts_edit_company():
     vendor = request.form['id']
     print(vendor)
@@ -126,23 +126,23 @@ def submit_contacts_edit_company():
     print(SMS)
     return company.editContact(mysql,vendor,contact,detail,whatsapp,viber,call,SMS)
 
-@app.route('/img/<path:filename>')  
+@application.route('/img/<path:filename>')  
 def send_file(filename):  
     return send_from_directory('images/', filename)
 
-@app.route('/delete_img' )
+@application.route('/delete_img' )
 def delete_img():
     id = request.args.get("id")
     vendor = request.args.get("vendor")
     return company.editDeleteImg(mysql,id,vendor)
 
-@app.route('/delete_phone')
+@application.route('/delete_phone')
 def delete_phone():
     id = request.args.get("id")
     vendor = request.args.get("vendor")
     return company.editDeleteContact(mysql, id, vendor)
    
-@app.route('/submit_message', methods=['POST'])
+@application.route('/submit_message', methods=['POST'])
 def submit_message():
     message = request.form['message']
     vendor_phone = request.form['vendor_phone']
@@ -150,4 +150,9 @@ def submit_message():
     user_phone = request.form['user_phone']
     return vendor.vendorSendMessage(mysql, message , vendor_phone , vendorid , user_phone)
 
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
 
--- database.py
+++ database.py
@@ -2,10 +2,10 @@
 from flask_mysqldb import MySQL
 
 def getDatabase(app):
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_HOST'] = 'shopup.c5qdsuoy5mft.ap-south-1.rds.amazonaws.com'
+    app.config['MYSQL_USER'] = 'admin'
     #app.config['MYSQL_PASSWORD'] = 'M@hpshinyblack'
-    app.config['MYSQL_PASSWORD'] = 'mysqlroot'
+    app.config['MYSQL_PASSWORD'] = 'ShopUpAdmin'
     app.config['MYSQL_DB'] = 'home_delivery'
 
     return MySQL(app)  
